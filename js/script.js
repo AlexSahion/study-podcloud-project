a029442713741c84a1e9d5bf57c40c9d6f20901d
AlexSahion <saha.las399@gmail.com>
2024-05-25 15:43:08 +0200

    new: functional for burger

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Burger Menu
-// const iconMenu = document.querySelector(".menu__icon")
-// const menuBody = document.querySelector(".menu__body")
-// if (iconMenu) {
-//   iconMenu.addEventListener('click', e => {
-//     document.body.classList.toggle("_active")
-//     iconMenu.classList.toggle("_active")
-//     menuBody.classList.toggle("_active")
-//   })
-// }
+const iconMenu = document.querySelector(".menu__icon")
+const menuBody = document.querySelector(".menu__body")
+if (iconMenu) {
+	iconMenu.addEventListener('click', e => {
+		document.body.classList.toggle("_lock")
+		iconMenu.classList.toggle("_active")
+		menuBody.classList.toggle("_active")
+	})
+}
